27eb7700a903b3eef5a711702853838c62912377
Johan9565 <johan_palma45@hotmail.com>
2023-05-25 18:36:29 -0500

    conexion base de datos test

--- test/unit/app/index.test.js
+++ test/unit/app/index.test.js
@@ -10,24 +10,6 @@ afterAll(()=>{  //hook
   server.close()
 })
 
-describe('Prueba que devolvera un status 200 para indicar que el servidor este en linea', () => {
-  test('Debe devolver status 200 ', async () => {
-     const response = await api.get('/')
-
-    expect(response.statusCode).toBe(200)
-  })
-  
-  test('Validar que funcione la paginación en BusinessEntity', async () =>{
-    const pag1 = await api.get('/api/businessentity/getlist/1')
-    const pag2 = await api.get('/api/businessentity/getlist/2')
-    const pag3 = await api.get('/api/businessentity/getlist/3')
-
-    expect(pag1.statusCode).toBe(200)
-    expect(pag2.statusCode).toBe(200)
-    expect(pag3.statusCode).not.toBe(200)
-    expect(pag3.statusCode).toBe(404)
-  } )
-})
 
 describe('Prueba de conexión a la base de datos.', () => {
 
